04e9d9d7f54386672fabf671f98c63598ae9cff0
Arvinder <arvinder_kamboo@hotmail.com>
2023-12-09 18:30:46 +0000

    Updates

--- stream.js
+++ stream.js
@@ -1,16 +1,21 @@
 var i = 0;
-
+var idValue;
+//var idValue;
 addEventListener("message", (message) => {
     if (message.data.command === "stream") {
+      idValue = message.data.idValue
         console.log(`Filename ${message.data.filename}`);
-        timedCount(message.data.filename);
+        timedCount();
+        //idValue = message.data.id;
     }
   });
 
 function timedCount() {
   i = i + 1;
-  postMessage(i);
+  postMessage({value : i, idValue : idValue});
   setTimeout("timedCount()",500);
+
+  // Use FileReader to get chunks and pass to server.
 }
 
 //timedCount(); 
