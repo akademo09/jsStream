d17040151d2479fe551e0dae08ed3aa41d0fe467
Arvinder <arvinder_kamboo@hotmail.com>
2023-12-09 19:19:27 +0000

    Tidy

--- stream.js
+++ stream.js
@@ -1,9 +1,11 @@
 var i = 0;
 var idValue;
+var filenameValue;
 //var idValue;
 addEventListener("message", (message) => {
     if (message.data.command === "stream") {
       idValue = message.data.idValue
+      filenameValue = message.data.filename;
         console.log(`Filename ${message.data.filename}`);
         timedCount();
         //idValue = message.data.id;
@@ -12,8 +14,8 @@ addEventListener("message", (message) => {
 
 function timedCount() {
   i = i + 1;
-  postMessage({value : i, idValue : idValue});
-  setTimeout("timedCount()",500);
+  postMessage({value : i, idValue : idValue, filenameValue : filenameValue});
+  setTimeout("timedCount()", 1000);
 
   // Use FileReader to get chunks and pass to server.
 }
